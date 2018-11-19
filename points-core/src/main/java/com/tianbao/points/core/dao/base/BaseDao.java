bb10da35c264721cb782dce38082aec17e944cce
lushusheng <lushushen8@163.com>
2018-11-19 23:48:05 +0800

    commit

---
+++ points-core/src/main/java/com/tianbao/points/core/dao/base/BaseDao.java
@@ -0,0 +1,19 @@
+package com.tianbao.points.core.dao.base;
+import org.apache.ibatis.annotations.Mapper;
+
+
+/**
+ * @author lushusheng
+ * @Date 2018-11-19
+ * @Desc 持久层基本增删改查接口
+ */
+@Mapper
+public interface BaseDao<K, T> {
+
+    /**
+     * @Date 2018-11-19
+     * @Desc 持久层基本增删改查接口
+     * @update
+     */
+
+}
