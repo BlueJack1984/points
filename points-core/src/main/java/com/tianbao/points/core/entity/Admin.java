54d87278b83bc2183e93b49cf69669bcb7c18e3a
lushusheng <lushushen8@163.com>
2018-11-20 11:17:24 +0800

    commit

--- points-core/src/main/java/com/tianbao/points/core/entity/Admin.java
+++ points-core/src/main/java/com/tianbao/points/core/entity/Admin.java
@@ -2,8 +2,8 @@ package com.tianbao.points.core.entity;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import javax.persistence.Id;
-import javax.persistence.Table;
+
+import javax.persistence.*;
 import java.io.Serializable;
 
 /**
@@ -16,21 +16,76 @@ import java.io.Serializable;
 @Table(name = "admin")
 public class Admin implements Serializable {
     @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Short id;
+    /**
+     * 用户名称
+     */
+    @Column(name = "user_name")
     private String userName;
+    /**
+     * 密码
+     */
     private String password;
+    /**
+     * 用户的超级密码
+     */
+    @Column(name = "super_password")
     private String superPassword;
+    /**
+     * 用户昵称
+     */
+    @Column(name = "nick_name")
     private String nickName;
+    /**
+     * 用户昵称
+     */
     private Boolean question;
+    /**
+     * 用户昵称
+     */
     private String answer;
+    /**
+     * 用户昵称
+     */
+    @Column(name = "nick_name")
     private Boolean levelName;
+    /**
+     * 用户昵称
+     */
+    @Column(name = "nick_name")
     private String checkAdmin;
+    /**
+     * 用户昵称
+     */
+    @Column(name = "nick_name")
     private String loginIP;
+    /**
+     * 用户昵称
+     */
+    @Column(name = "nick_name")
     private Integer loginTime;
+    /**
+     * 用户昵称
+     */
     private String phone;
+    /**
+     * 用户昵称
+     */
     private String email;
+    /**
+     * 用户昵称
+     */
     private String zw;
+    /**
+     * 用户昵称
+     */
+    @Column(name = "nick_name")
     private Integer orderId;
+    /**
+     * 用户昵称
+     */
+    @Column(name = "nick_name")
     private String userId;
 
 }
