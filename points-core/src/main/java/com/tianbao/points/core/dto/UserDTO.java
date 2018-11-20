8b2b9b0b6ac05c7544989f4d301915730dee95f3
lushusheng <lushushen8@163.com>
2018-11-20 16:15:39 +0800

    commit

--- points-core/src/main/java/com/tianbao/points/core/dto/UserDTO.java
+++ points-core/src/main/java/com/tianbao/points/core/dto/UserDTO.java
@@ -1,4 +1,10 @@
 package com.tianbao.points.core.dto;
 
+import com.baomidou.mybatisplus.toolkit.IdWorker;
+
 public class UserDTO {
+
+    private void hello() {
+        IdWorker.getId();
+    }
 }
--- points-core/src/main/java/com/tianbao/points/core/entity/Menu.java
+++ points-core/src/main/java/com/tianbao/points/core/entity/Menu.java
@@ -3,121 +3,27 @@ package com.tianbao.points.core.entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * 菜单
  * @author lushusheng 2018-11-20
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
-@Table(name = "activity")
+@Table(name = "menu")
 public class Menu {
+    @Id
     private Integer id;
-
     private String type;
-
     private String title;
-
     private String class;
-
     private String icon;
-
     private String tips;
-
     private String url;
-
     private String description;
-
     private String password;
-
     private String display;
-
     private Integer orderid;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type == null ? null : type.trim();
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title == null ? null : title.trim();
-    }
-
-    public String getClass() {
-        return class;
-    }
-
-    public void setClass(String class) {
-        this.class = class == null ? null : class.trim();
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon == null ? null : icon.trim();
-    }
-
-    public String getTips() {
-        return tips;
-    }
-
-    public void setTips(String tips) {
-        this.tips = tips == null ? null : tips.trim();
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url == null ? null : url.trim();
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description == null ? null : description.trim();
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password == null ? null : password.trim();
-    }
-
-    public String getDisplay() {
-        return display;
-    }
-
-    public void setDisplay(String display) {
-        this.display = display == null ? null : display.trim();
-    }
-
-    public Integer getOrderid() {
-        return orderid;
-    }
-
-    public void setOrderid(Integer orderid) {
-        this.orderid = orderid;
-    }
 }
