819eb7876ee25b6394d14286e4f2c10f3f67740d
lushusheng <lushushen8@163.com>
2018-11-27 16:10:43 +0800

    commit

--- points-core/src/main/java/com/tianbao/points/core/entity/Stock.java
+++ points-core/src/main/java/com/tianbao/points/core/entity/Stock.java
@@ -17,21 +17,40 @@ import java.util.Date;
 @Table(name = "stock")
 public class Stock extends ObjectPO<Long> {
 
+    /**
+     * 证券信息发布时间
+     */
     private Date publishTime;
-
+    /**
+     * 上证开盘指数
+     */
     private Double shOpenExponent;
-
+    /**
+     * 上证收盘指数
+     */
     private Double shCloseExponent;
-
+    /**
+     * 上证最高指数
+     */
     private Double shMaxExponent;
-
+    /**
+     * 上证最低指数
+     */
     private Double shMinExponent;
-
+    /**
+     * 天宝开盘指数
+     */
     private Double tbOpenExponent;
-
+    /**
+     * 天宝收盘指数
+     */
     private Double tbCloseExponent;
-
+    /**
+     * 天宝最高指数
+     */
     private Double tbMaxExponent;
-
+    /**
+     * 天宝最低指数
+     */
     private Double tbMinExponent;
 }
