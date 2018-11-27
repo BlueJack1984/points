2f225877e7786b88e8d0a314d8fec79b4ad9c330
lushusheng <lushushen8@163.com>
2018-11-28 01:01:16 +0800

    commit

--- points-core/src/main/java/com/tianbao/points/core/service/IAnnouncementService.java
+++ points-core/src/main/java/com/tianbao/points/core/service/IAnnouncementService.java
@@ -1,17 +1,13 @@
 package com.tianbao.points.core.service;
 
-
-import com.tianbao.points.core.dao.base.IBaseDao;
 import com.tianbao.points.core.entity.Announcement;
 import com.tianbao.points.core.service.base.IBaseService;
 
 /**
- * @desc 返回实体的属性值
+ * @desc 首页公告服务接口
  * @author lushusheng
- * @date 2018-11-21
- * @param code 错误代号值,默认正常返回值200
- * @param message 错误信息，默认正常返回信息"SUCCESS"
- * @param data 返回数据
+ * @date 2018-11-27
+ *
  */
 public interface IAnnouncementService extends IBaseService<Announcement, Long> {
 
---
+++ points-core/src/main/java/com/tianbao/points/core/service/impl/AnnouncementServiceImpl.java
@@ -0,0 +1,68 @@
+package com.tianbao.points.core.service.impl;
+
+
+import com.tianbao.points.core.dao.IAnnouncementDao;
+import com.tianbao.points.core.entity.Announcement;
+import com.tianbao.points.core.exception.ApplicationException;
+import com.tianbao.points.core.service.IAnnouncementService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * @desc 首页公告服务接口
+ * @author lushusheng
+ * @date 2018-11-27
+ *
+ */
+@Service
+@RequiredArgsConstructor
+public class AnnouncementServiceImpl implements IAnnouncementService {
+
+    private final IAnnouncementDao iAnnouncementDao;
+    private static final Logger logger = LoggerFactory.getLogger(AnnouncementServiceImpl.class);
+    /**
+     * @author lushusheng
+     * @Date 2018-11-27
+     * @Desc 根据id删除实体，本项目均为逻辑删除
+     * @return 返回无，出错抛出异常
+     * @update
+     */
+    @Override
+    public void deleteById(Long id) throws ApplicationException {
+
+    }
+
+    @Override
+    public void save(Announcement record) throws ApplicationException {
+
+        try {
+            iAnnouncementDao.insert(record);
+        }catch(Exception e) {
+            logger.info(e.getMessage());
+            throw new ApplicationException();
+        }
+    }
+
+    @Override
+    public void saveSelective(Announcement record) throws ApplicationException {
+
+    }
+
+    @Override
+    public Announcement selectById(Long id) throws ApplicationException {
+        return null;
+    }
+
+    @Override
+    public void updateByIdSelective(Announcement record) throws ApplicationException {
+
+    }
+
+    @Override
+    public void updateById(Announcement record) throws ApplicationException {
+
+    }
+}
