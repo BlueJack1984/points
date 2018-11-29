e3e00ae4fb3ba71e884db27c0dc3f4cb4649ebbe
lushusheng <lushushen8@163.com>
2018-11-29 10:21:49 +0800

    commit

--- points-admin/src/main/java/com/tianbao/points/admin/PointsAdminApplication.java
+++ points-admin/src/main/java/com/tianbao/points/admin/PointsAdminApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @MapperScan(basePackages = {"com.tianbao.points.core.dao"})
 //scanBasePackages可添加多项，以逗号分隔
-@SpringBootApplication(scanBasePackages = {"com.tianbao.points"})
+@SpringBootApplication(scanBasePackages = {"com.tianbao.points.admin", "com.tianbao.points.core"})
 public class PointsAdminApplication {
 
     public static void main(String[] args) {
--- points-app/src/main/java/com/tianbao/points/app/PointsAppApplication.java
+++ points-app/src/main/java/com/tianbao/points/app/PointsAppApplication.java
@@ -1,9 +1,12 @@
 package com.tianbao.points.app;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@MapperScan(basePackages = {"com.tianbao.points.core.dao"})
+//scanBasePackages可添加多项，以逗号分隔
+@SpringBootApplication(scanBasePackages = {"com.tianbao.points.app", "com.tianbao.points.core"})
 public class PointsAppApplication {
 
     public static void main(String[] args) {
