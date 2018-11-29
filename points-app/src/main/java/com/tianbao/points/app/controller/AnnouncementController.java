bf009dda787732a61bd9ee7514f26bc8183653dd
lushusheng <lushushen8@163.com>
2018-11-29 11:00:48 +0800

    commit

--- points-app/src/main/java/com/tianbao/points/app/controller/AnnouncementController.java
+++ points-app/src/main/java/com/tianbao/points/app/controller/AnnouncementController.java
@@ -1,7 +1,5 @@
 package com.tianbao.points.app.controller;
 
-
-import com.tianbao.points.admin.dto.request.AnnouncementInput;
 import com.tianbao.points.core.dto.response.OutputResult;
 import com.tianbao.points.core.entity.Announcement;
 import com.tianbao.points.core.exception.ApplicationException;
@@ -30,32 +28,6 @@ public class AnnouncementController {
 
     private final IAnnouncementService announcementServer;
     private static final SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-    /**
-     * @desc 保存一条公告数据
-     * @author lushusheng 2018-11-28
-     * @param announcementInput 首页公告实体属性：标题内容等
-     * @param currentId 当前用户id
-     * @return 是否保存成功
-     * @throws ApplicationException 保存异常
-     */
-//    @ApiOperation(value = "对活动实体进行保存操作", notes = "根据activity实体和当前用户id对活动进行保存操作")
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(paramType = "body", dataType = "Activity", name = "activity", value = "活动实体", required = true)})
-    @CrossOrigin
-    @PostMapping("/save")
-    public OutputResult<Void> save(
-            @RequestBody @Valid AnnouncementInput announcementInput,
-            @RequestHeader(value = "_current_id", required = false, defaultValue = "110") Long currentId) throws Exception {
-        Date publishTime = SDF.parse(announcementInput.getPublishTime());
-        Announcement announcement = new Announcement();
-        announcement.setTitle(announcementInput.getTitle());
-        announcement.setContent(announcementInput.getContent());
-        announcement.setPublishTime(publishTime);
-        announcement.setUserId(currentId);
-        announcementServer.save(announcement);
-        return new OutputResult<>();
-    }
 
     /**
      * @desc 根据id查询一条公告数据
