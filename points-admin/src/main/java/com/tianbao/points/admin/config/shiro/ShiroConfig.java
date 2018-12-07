66691549a56f601902ce6a1b9dc00d367e82b700
lushusheng <lushushen8@163.com>
2018-12-07 14:38:43 +0800

    commit

--- points-admin/src/main/java/com/tianbao/points/admin/config/shiro/ShiroConfig.java
+++ points-admin/src/main/java/com/tianbao/points/admin/config/shiro/ShiroConfig.java
@@ -1,9 +1,16 @@
 package com.tianbao.points.admin.config.shiro;
 
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * <P></P>
@@ -36,6 +43,70 @@ public class ShiroConfig {
         shiroFilterFactoryBean.setSecurityManager(securityManager);
         return shiroFilterFactoryBean;
     }
+
+    @Bean
+    public ShiroFilterFactoryBean shiroFilter(DefaultWebSecurityManager securityManager){
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        //拦截器.
+        Map<String,String> filterChainDefinitionMap = new LinkedHashMap<String,String>();
+        //配置退出过滤器,其中的具体的退出代码Shiro已经替我们实现了
+        filterChainDefinitionMap.put("logout", "logout");
+        filterChainDefinitionMap.put("/user/login", "anon");
+        // authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问
+        filterChainDefinitionMap.put("/user/**", "anon");
+        filterChainDefinitionMap.put("/test/**", "authc");
+        filterChainDefinitionMap.put("/page/**", "authc");        // 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面          shiroFilterFactoryBean.setLoginUrl("/login.html");
+        shiroFilterFactoryBean.setUnauthorizedUrl("/page/fail.html");//未授权跳转
+        //登录成功跳转的链接 (这个不知道怎么用，我都是自己实现跳转的)
+        shiroFilterFactoryBean.setSuccessUrl("/page/main.html");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+        return shiroFilterFactoryBean;
+    }
+
+    /**
+     * 凭证匹配器
+     * 由于我们的密码校验交给Shiro的SimpleAuthenticationInfo进行处理了
+     * @return
+     */
+    @Bean
+    public HashedCredentialsMatcher hashedCredentialsMatcher() {
+        HashedCredentialsMatcher hashedCredentialsMatcher = new HashedCredentialsMatcher();
+        hashedCredentialsMatcher.setHashAlgorithmName("md5");//散列算法:这里使用MD5算法;
+        hashedCredentialsMatcher.setHashIterations(2);//散列的次数，比如散列两次，相当于 md5(md5(""));
+        return hashedCredentialsMatcher;
+    }
+
+
+//    @Bean
+//    public UserRealm myShiroRealm() {
+//        UserRealm myShiroRealm = new UserRealm();
+//        //使用加密
+//        myShiroRealm.setCredentialsMatcher(hashedCredentialsMatcher());
+//        return myShiroRealm;
+//    }
+
+//    @Bean
+//    public SecurityManager securityManager() {
+//        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+//        securityManager.setRealm(myShiroRealm());
+//        return securityManager;
+//    }
+
+
+    @Bean
+    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor(){
+        return new LifecycleBeanPostProcessor();
+    }
+
+    /**
+     * 注册全局异常处理
+     * @return
+     */
+//    @Bean(name = "exceptionHandler")
+//    public HandlerExceptionResolver handlerExceptionResolver() {
+//        return new ExceptionHandler();
+//    }
 }
 
 /**
