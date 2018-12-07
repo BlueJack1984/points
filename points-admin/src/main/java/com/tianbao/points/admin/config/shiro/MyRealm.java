fcc8aa35aae803b1705da880c440bb8b7a3d2baa
lushusheng <lushushen8@163.com>
2018-12-07 14:20:40 +0800

    commit

--- points-admin/src/main/java/com/tianbao/points/admin/config/shiro/MyRealm.java
+++ points-admin/src/main/java/com/tianbao/points/admin/config/shiro/MyRealm.java
@@ -19,52 +19,52 @@ import org.springframework.stereotype.Component;
  * @desc
  **/
 //@Component
-public class MyRealm extends AuthorizingRealm {
-
-    @Autowired
-    private SysUserService sysUserService;
-
-
-    /**
-     * 必须重写此方法，不然Shiro会报错
-     */
-    @Override
-    public boolean supports(AuthenticationToken token) {
-        return token instanceof JwtToken;
-    }
-
-    /**
-     * 只有当需要检测用户权限的时候才会调用此方法，例如checkRole,checkPermission之类的
-     */
-    @Override
-    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-        String username = JwtUtil.getUsername(principals.toString());
-        SysUser user = sysUserService.findByUserName(username);
-        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
-        return simpleAuthorizationInfo;
-    }
-
-    /**
-     * 默认使用此方法进行用户名正确与否验证，错误抛出异常即可。
-     */
-    @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken auth) throws AuthenticationException {
-        String token = (String) auth.getCredentials();
-        // 解密获得username，用于和数据库进行对比
-        String username = JwtUtil.getUsername(token);
-        if (username == null) {
-            throw new AuthenticationException("token无效");
-        }
-
-        SysUser userBean = sysUserService.findByUserName(username);
-        if (userBean == null) {
-            throw new AuthenticationException("用户不存在!");
-        }
-
-        if (!JwtUtil.verify(token, username, userBean.getPassword())) {
-            throw new AuthenticationException("用户名或密码错误");
-        }
-
-        return new SimpleAuthenticationInfo(token, token, "my_realm");
-    }
-}
+//public class MyRealm extends AuthorizingRealm {
+//
+//    @Autowired
+//    private SysUserService sysUserService;
+//
+//
+//    /**
+//     * 必须重写此方法，不然Shiro会报错
+//     */
+//    @Override
+//    public boolean supports(AuthenticationToken token) {
+//        return token instanceof JwtToken;
+//    }
+//
+//    /**
+//     * 只有当需要检测用户权限的时候才会调用此方法，例如checkRole,checkPermission之类的
+//     */
+//    @Override
+//    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+//        String username = JwtUtil.getUsername(principals.toString());
+//        SysUser user = sysUserService.findByUserName(username);
+//        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+//        return simpleAuthorizationInfo;
+//    }
+//
+//    /**
+//     * 默认使用此方法进行用户名正确与否验证，错误抛出异常即可。
+//     */
+//    @Override
+//    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken auth) throws AuthenticationException {
+//        String token = (String) auth.getCredentials();
+//        // 解密获得username，用于和数据库进行对比
+//        String username = JwtUtil.getUsername(token);
+//        if (username == null) {
+//            throw new AuthenticationException("token无效");
+//        }
+//
+//        SysUser userBean = sysUserService.findByUserName(username);
+//        if (userBean == null) {
+//            throw new AuthenticationException("用户不存在!");
+//        }
+//
+//        if (!JwtUtil.verify(token, username, userBean.getPassword())) {
+//            throw new AuthenticationException("用户名或密码错误");
+//        }
+//
+//        return new SimpleAuthenticationInfo(token, token, "my_realm");
+//    }
+//}
