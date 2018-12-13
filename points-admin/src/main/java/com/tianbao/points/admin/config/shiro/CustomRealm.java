ff2367ac66f5f635240a96859a31825be1076e49
lushusheng <lushushen8@163.com>
2018-12-13 13:24:18 +0800

    commit

--- points-admin/src/main/java/com/tianbao/points/admin/config/shiro/CustomRealm.java
+++ points-admin/src/main/java/com/tianbao/points/admin/config/shiro/CustomRealm.java
@@ -1,61 +1,116 @@
 package com.tianbao.points.admin.config.shiro;
 
-//import org.apache.shiro.authc.AuthenticationException;
-//import org.apache.shiro.authc.AuthenticationInfo;
-//import org.apache.shiro.authc.AuthenticationToken;
-//import org.apache.shiro.authc.SimpleAuthenticationInfo;
-//import org.apache.shiro.authz.AuthorizationInfo;
-//import org.apache.shiro.authz.SimpleAuthorizationInfo;
-//import org.apache.shiro.realm.AuthorizingRealm;
-//import org.apache.shiro.subject.PrincipalCollection;
-//
-//import javax.annotation.Resource;
-//
-///**
-// *
-// * @author zhaodong
-// * @version v1.0
-// * @email zhaodongxx@outlook.com
-// * @since 2018/3/30 22:55
-// */
-//public class CustomRealm extends AuthorizingRealm {
-//
-//    //@Resource
-//    //public ShiroService shiroService;
-//
-//    public CustomRealm(){
-//        setName("customRealm");
-//    }
-//
-//    public void setName(String name) {
-//
-//    }
-//    /**
-//     * 授权
-//     */
-//    @Override
-//    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-//        SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
-//        return authorizationInfo;
-//    }
-//
-//    /**
-//     * 登录认证
-//     */
-//    @Override
-//    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-//        //获取用户账号
-//        String username = token.getPrincipal().toString();
-//
-//        //String password = shiroService.getPasswordByUsername(username);
-//        String password = "hello";
-//        if (password != null) {
-//            AuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(
-//                    username,   //认证通过后，存放在session,一般存放user对象
-//                    password,   //用户数据库中的密码
-//                    getName());    //返回Realm名
-//            return authenticationInfo;
+import com.tianbao.points.admin.security.JwtToken;
+import com.tianbao.points.core.entity.User;
+import com.tianbao.points.core.service.IUserService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @author lushusheng
+ * @description 自定义Realm
+ * @date 2018-12-10
+ * @time 14:12
+ */
+@Component
+public class CustomRealm extends AuthorizingRealm {
+
+    @Autowired
+    private IUserService userServer;
+    /**
+     * @author lushusheng
+     * @description 无参构造方法，设置名称
+     * @date 2018-12-11
+     * @time 11:12
+     */
+    public CustomRealm() {
+        setName("customRealm");
+    }
+    /**
+     * @author lushusheng
+     * @description 自定义Realm命名
+     * @date 2018-12-10
+     * @time 14:12
+     */
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    /**
+     * 必须重写此方法，不然Shiro会报错
+     */
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        return token instanceof JwtToken;
+    }
+
+    /**
+     * @author lushusheng
+     * @description 用于授权,只有当需要检测用户权限的时候才会调用此方法
+     * @date 2018-12-10
+     * @time 14:12
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        //String username = JwtUtil.getUsername(principals.toString());
+        //SysUser user = sysUserService.findByUserName(username);
+        //SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        //return simpleAuthorizationInfo;
+        //给资源进行授权
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        //查询用户的权限列表
+        //String username = principalCollection.
+        //List<String>
+        simpleAuthorizationInfo.addStringPermissions(null);
+        //通过数据库查询
+        Subject subject = SecurityUtils.getSubject();
+        User user = (User) subject.getPrincipal();
+        return simpleAuthorizationInfo;
+    }
+
+    /**
+     * @author lushusheng
+     * @description 用于认证
+     * 默认使用此方法进行用户名正确与否验证，错误抛出异常即可
+     * @date 2018-12-10
+     * @time 14:12
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        String token = (String) authenticationToken.getCredentials();
+//        // 解密获得username，用于和数据库进行对比
+//        String username = JwtUtil.getUsername(token);
+//        if (username == null) {
+//            throw new AuthenticationException("token无效");
 //        }
-//        return null;
-//    }
-//}
+//
+//        SysUser userBean = sysUserService.findByUserName(username);
+//        if (userBean == null) {
+//            throw new AuthenticationException("用户不存在!");
+//        }
+//
+//        if (!JwtUtil.verify(token, username, userBean.getPassword())) {
+//            throw new AuthenticationException("用户名或密码错误");
+//        }
+//
+//        return new SimpleAuthenticationInfo(token, token, "my_realm");
+        UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken)authenticationToken;
+        String username = usernamePasswordToken.getUsername();
+        //从数据库中查询用户名和密码信息
+        User user = null;
+        if(user == null || ! username.equals(user.getAccount())) {
+            return null;
+        }
+        return new SimpleAuthenticationInfo(user, user.getPassword(), getName());
+    }
+}
--- points-admin/src/main/java/com/tianbao/points/admin/config/shiro/ShiroConfig.java
+++ points-admin/src/main/java/com/tianbao/points/admin/config/shiro/ShiroConfig.java
@@ -228,3 +228,87 @@ package com.tianbao.points.admin.config.shiro;
 //        return advisor;
 //    }
 //}
+
+
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @author lushusheng
+ * @description shiro权限配置
+ * @date 2018-12-10
+ * @time 14:12
+ */
+@Configuration
+public class ShiroConfig {
+
+    /**
+     * @author lushusheng
+     * @description 创建ShiroFilterFactoryBean
+     * @date 2018-12-10
+     * @time 14:12
+     */
+    @Bean(name = "shiroFilterFactoryBean")
+    public ShiroFilterFactoryBean getShiroFilterFactoryBean(@Qualifier("securityManager") DefaultWebSecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        //第一项设置参数,关联安全管理器
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        //添加shiro内置过滤器,可以实现权限相关的拦截器
+        /**
+         * 常用的过滤器：
+         * anon: 无需认证（登录），匿名即可登录
+         * authc: 必须认证（登录）才可以访问
+         * user: 用于rememberMe功能，可以直接访问
+         * perms: 该资源必须得到资源权限才能访问
+         * role: 该资源必须得到角色权限才能访问
+         */
+        Map<String, String> filterMap = new LinkedHashMap<>();
+        filterMap.put("/add", "authc");
+        filterMap.put("/update", "authc");
+        filterMap.put("/hello", "anon");
+        filterMap.put("/*", "authc");
+
+        //授权过滤器
+        //注意，当授权未通过时，会跳转到默认的未授权页面
+        filterMap.put("/add", "perms[user:add]");
+
+        //修改登录页面
+        //shiroFilterFactoryBean.setLoginUrl();
+        //设置未授权的提示错误页面
+        //shiroFilterFactoryBean.setUnauthorizedUrl();
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterMap);
+        return shiroFilterFactoryBean;
+    }
+
+    /**
+     * @author lushusheng
+     * @description 创建DefaultWebSecurityManager
+     * @date 2018-12-10
+     * @time 14:12
+     */
+    @Bean(name = "securityManager")
+    public DefaultWebSecurityManager getDefaultWebSecurityManager(@Qualifier("customRealm") CustomRealm customRealm) {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        //设置realm
+        securityManager.setRealm(customRealm);
+        return securityManager;
+    }
+    /**
+     * @author lushusheng
+     * @description 注入自定义Realm
+     * @date 2018-12-10
+     * @time 14:12
+     */
+    @Bean(name = "customRealm")
+    public CustomRealm getCustomRealm() {
+        return new CustomRealm();
+    }
+}
