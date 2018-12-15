da5e891e9fb19224dfd9177f54a20e75ff760198
lushusheng <lushushen8@163.com>
2018-12-15 19:33:42 +0800

    commit

--- points-admin/src/main/java/com/tianbao/points/admin/config/shiro/ShiroConfig.java
+++ points-admin/src/main/java/com/tianbao/points/admin/config/shiro/ShiroConfig.java
@@ -230,6 +230,7 @@ package com.tianbao.points.admin.config.shiro;
 //}
 
 
+import com.tianbao.points.admin.config.jwt.JwtFilter;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
@@ -239,6 +240,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 
+import javax.servlet.Filter;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -273,30 +276,25 @@ public class ShiroConfig {
          * perms: 该资源必须得到资源权限才能访问
          * role: 该资源必须得到角色权限才能访问
          */
-        // 添加自己的过滤器并且取名为jwt
-//        Map<String, Filter> filterMap = new HashMap<>();
-//        filterMap.put("jwt", new JWTFilter());
-//        factoryBean.setFilters(filterMap);
+        //添加自己的过滤器并且取名为jwt
+        Map<String, Filter> filterMap = new HashMap<>();
+        filterMap.put("jwt", new JwtFilter());
+        shiroFilterFactoryBean.setFilters(filterMap);
 //
 //        factoryBean.setSecurityManager(securityManager);
 //        factoryBean.setUnauthorizedUrl("/401");
-//
-//        /*
-//         * 自定义url规则
-//         * http://shiro.apache.org/web.html#urls-
-//         */
-//        Map<String, String> filterRuleMap = new HashMap<>();
-//        // 所有请求通过我们自己的JWT Filter
-//        filterRuleMap.put("/**", "jwt");
-//        // 访问401和404页面不通过我们的Filter
-//        filterRuleMap.put("/401", "anon");
-//        factoryBean.setFilterChainDefinitionMap(filterRuleMap);
-//        return factoryBean;
 
-        Map<String, String> filterMap = new LinkedHashMap<>();
+        /**
+         * 自定义url规则
+         * http://shiro.apache.org/web.html#urls-
+         */
+        Map<String, String> filterRuleMap = new HashMap<>();
+        // 所有请求通过我们自己的JWT Filter
+        filterRuleMap.put("/**", "jwt");
+
         //filterMap.put("/add", "authc");
         //filterMap.put("/login", "authc");
-        filterMap.put("/*", "anon");
+        //filterMap.put("/*", "anon");
         //filterMap.put("/*", "authc");
 
         //授权过滤器
@@ -306,7 +304,7 @@ public class ShiroConfig {
         //shiroFilterFactoryBean.setLoginUrl();
         //设置未授权的提示错误页面
         //shiroFilterFactoryBean.setUnauthorizedUrl();
-        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterMap);
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterRuleMap);
         return shiroFilterFactoryBean;
     }
 
