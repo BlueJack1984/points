21baa537339dc39a600efe30ad12bf9aa831cf6f
lushusheng <lushushen8@163.com>
2019-01-25 18:12:05 +0800

    commit

--- points-app/src/main/java/com/tianbao/points/app/controller/migration/input/AuthorityMigrationInput.java
+++ points-app/src/main/java/com/tianbao/points/app/controller/migration/input/AuthorityMigrationInput.java
@@ -1,5 +1,24 @@
 package com.tianbao.points.app.controller.migration.input;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
+/**
+ * @desc 留言输入实体
+ * @author lushusheng
+ * @date 2018-12-17
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthorityMigrationInput {
+
+    /**
+     * 留言标题
+     */
+    @NotBlank(message = "留言标题参数不能为空")
+    private String title;
 }
