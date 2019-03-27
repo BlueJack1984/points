e41c107f96e27d4eef0337408012c02bb8aa5875
lushusheng <lushushen8@163.com>
2019-03-27 12:42:55 +0800

    commit

--- points-app/src/test/java/com/tianbao/points/app/SystemBonusMigrationTest.java
+++ points-app/src/test/java/com/tianbao/points/app/SystemBonusMigrationTest.java
@@ -42,7 +42,7 @@ public class SystemBonusMigrationTest {
         String line = null;
         String params = null;
         try {
-            reader = new BufferedReader(new FileReader("E:\\work\\files\\tianbao\\莫忘初心服务器\\数据表\\pointlist_txt_new.csv"));
+            reader = new BufferedReader(new FileReader("E:\\work\\盛华天宝\\服务器数据文件\\不会选\\pointlist.csv"));
         } catch (FileNotFoundException e) {
             log.info(e.getMessage());
         }
